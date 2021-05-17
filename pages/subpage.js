44d213f70605a5948e238532be41a39b4fbf63ae
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2021-05-17 13:46:52 +0200

    clear template and test deployment

---
+++ pages/subpage.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Subpage = () => {
+  return (
+    <>
+      <h1>I am subpage</h1>
+      <Link href="/">Home</Link>
+    </>
+  )
+}
+
+export default Subpage;

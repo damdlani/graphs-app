f3a2aa412088be0334eea9e935ed03ccc461e6b3
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2021-05-17 21:47:18 +0200

    fetch any data from gql

--- pages/public.js
+++ pages/public.js
@@ -1,7 +1,34 @@
-const PublicGraphPage = () => {
-  return (
-    <p>I am public graph page</p>
-  )
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
+const rocketQuery = gql`
+{
+  launchesPast(limit: 100) {
+    launch_date_local
+    rocket {
+      second_stage {
+        payloads {
+          payload_mass_kg
+        }
+      }
+    }
+  }
 }
+`;
+
+const PublicGraphPage = () => {
+  const {data, error, loading}  = useQuery(rocketQuery);
+  console.log(data)
+
+  if(loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      {data?.launchesPast.map((launch) => {
+        return <div>{launch.rocket.second_stage.payloads[0].payload_mass_kg} kg</div>
+      })}
+    </>
+  );
+};
 
 export default PublicGraphPage;

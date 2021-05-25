ad9c32340f196501657e1496e45c234f12fb9ce5
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2021-05-25 10:06:28 +0200

    generate static page with apollo query

--- pages/public.js
+++ pages/public.js
@@ -1,34 +1,28 @@
-import { useQuery } from "@apollo/client";
-import gql from "graphql-tag";
+import client from "../apollo-client";
+import { rocketQuery } from "../queries/rocketQuery";
 
-const rocketQuery = gql`
-{
-  launchesPast(limit: 100) {
-    launch_date_local
-    rocket {
-      second_stage {
-        payloads {
-          payload_mass_kg
-        }
-      }
-    }
-  }
-}
-`;
-
-const PublicGraphPage = () => {
-  const {data, error, loading}  = useQuery(rocketQuery);
-  console.log(data)
-
-  if(loading) return <p>Loading...</p>;
-
+const PublicGraphPage = ({ data }) => {
   return (
     <>
       {data?.launchesPast.map((launch) => {
-        return <div>{launch.rocket.second_stage.payloads[0].payload_mass_kg} kg</div>
+        return (
+          <div>{launch.rocket.second_stage.payloads[0].payload_mass_kg} kg</div>
+        );
       })}
     </>
   );
 };
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: rocketQuery,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
 export default PublicGraphPage;
